67999b68090c6d24a507839a89a4c0308eec383d
dragonman234893 <dakshdragonman@gmail.com>
2024-08-03 22:08:55 +0530

    Add files via upload

---
+++ syntaxcommenting.js
@@ -0,0 +1,2 @@
+document.getElementById('h').innerHTML="daksh"
+document.getElementById('p').innerHTML="hi"
---
+++ variables.js
@@ -0,0 +1,5 @@
+var x,y,z
+x=12
+y=30
+z=x+y
+document.getElementById("demo").innerHTML="the value of z=",z
